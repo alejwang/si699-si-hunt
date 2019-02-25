3bec4093659bd269ca29387604b26e7c769b948b
Alejandro Wang <me@alej.wang>
2019-02-25 01:47:42 -0500

    + Backend adjusted for peverywhere

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,6 +5,8 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+from db import db
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.event import Event, EventList
@@ -12,28 +14,30 @@ from resources.organizer import Organizer, OrganizerList
 from resources.location import Location, LocationList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'team_fancyfancy'
-api = Api(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://alejwang:****PASSWORD****@alejwang.mysql.pythonanywhere-services.com/alejwang$si699" # REMEMBER TO CHANGE THE PASSWORD!
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'team_****PASSWORD****' # REMEMBER TO CHANGE THE PASSWORD!
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # this line is for local debugging only
+
+db.init_app(app)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity)
-
-
+api = Api(app)
 api.add_resource(Event, '/event/<string:name>')
 api.add_resource(EventList, '/events')
 api.add_resource(Organizer, '/organizer/<string:name>')
 api.add_resource(OrganizerList, '/organizers')
 api.add_resource(Location, '/location/<string:name>')
 api.add_resource(LocationList, '/locations')
-
 api.add_resource(UserRegister, '/register')
 
+jwt = JWT(app, authenticate, identity)
+
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True) # this line is for local debugging only
